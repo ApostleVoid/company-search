26a6f1b58920cb2dae869ac796f8c578bfc3062d
ApostleVoid <apostle.void@proton.me>
2026-08-18 11:33:51 +0600

    Change API_KEY to be a global variable
    Expose API_KEY as a global variable on the window object.

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // config.js
 const ENCODED_KEY = "QVEuQWI4Uk42SzBlaHZoOF8tRGYwWmhJczF6YUV3cl9HY2M4d1VlQkpOVHdfSnN0elF2c2c=";
-const API_KEY = atob(ENCODED_KEY);
+window.API_KEY = atob(ENCODED_KEY);
